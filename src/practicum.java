31fcfa703c213ddca967a37050b7bcf6f43eccf8
Kevin <kgeurts@gmail.com>
2018-09-28 11:53:04 +0200

    Initial commit

---
+++ src/practicum.java
@@ -0,0 +1,29 @@
+public class practicum {
+}
+
+    public static void main(String[] args) {
+        {
+
+            boolean KevinAanUit = true ;
+            String mijnTekst = "Wat leuk";
+            String nogMeerTekst = "echt he! :O";
+
+            String samenTekst = mijnTekst + nogMeerTekst;
+
+            System.out.println( KevinAanUit );
+            System.out.println( samenTekst );
+
+            char eenLetter = 'c';
+
+            int getalA = 123;
+
+            float getalPunt = 1.0f; // of ook 1f
+
+            double getalPuntTwee = 1.0d;
+
+            System.out.println( KevinAanUit);
+            System.out.println( samenTekst );
+            System.out.println( getalA + getalPunt + getalPuntTwee);
+
+        }
+    }}
